6c0af25f28b4a0efb8a414cd152d02aeb6abdc1e
doodles172 <ash@duckorg.com>
2026-07-27 10:42:08 -0700

    fix(meta): some more values

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -5,6 +5,9 @@ export function meta({}: Route.MetaArgs) {
   return [
     { title: "Vantern" },
     { property: "og:image", content: "https://vantern.org/LargeVanternBanner.png" },
+    { property: "og:image:width", content: "1360" },
+    { property: "og:image:height", content: "660" },
+    { name: "twitter:card", content: "summary_large_image" },
     { name: "description", content: "FOSS organization, creating open-source software for others." },
   ];
 }
